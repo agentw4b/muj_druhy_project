910548a88d6e2f50a540af1ee412a5c356c69f5b
agentw4b <lubomir.klubus@gmail.com>
2021-11-11 16:42:11 +0100

    11.11.2021 16:42

--- druhy_project.py
+++ druhy_project.py
@@ -1,19 +1,19 @@
 playing_stones: list = ["X", "O"]
-initial_char = '_'
+initial_char = ' '
 
 
-def create_array (width:int, height:int):
+def create_array(width:int, height:int):
     array = []
     for _  in range (height):
         array.append(list(initial_char * width))
     return array
 
-def print_array (array:list):
+def print_array(array:list):
     print(len(array))
-    for i in range(0,len(array)):
+    for line in range(0,len(array)):
+        print(f'{array[line]}\n')
 
-        print(f'{array[i]}\n')
-
+#main program
 array1 =[]
 array1 = create_array (3,3)
 print_array(array1)
